16df16435071bc9e7f3842f4644c9d726af9d87c
Vasi85 <101883373+Vasi85@users.noreply.github.com>
2026-02-20 14:54:02 +0200

    Add files via upload

---
+++ 01-Programming Fundamentals and Unit Testing/01-Exercises/02-KeyConcepts/03Market/Program.cs
@@ -0,0 +1,12 @@
+﻿
+double tomatoPrice = double.Parse(Console.ReadLine());
+double tomatoQuantity = double.Parse(Console.ReadLine());
+double cucumberPrice = double.Parse(Console.ReadLine());
+double cucumberQuantity = double.Parse(Console.ReadLine());
+
+double tomatoCost = tomatoPrice * tomatoQuantity;
+double cucumberCost = cucumberPrice * cucumberQuantity;
+
+double totalCost = tomatoCost + cucumberCost;
+
+Console.WriteLine($"{totalCost:F2}");
---
+++ 01-Programming Fundamentals and Unit Testing/01-Exercises/02-KeyConcepts/04Tiles/Program.cs
@@ -0,0 +1,13 @@
+﻿
+double bathroomW = double.Parse(Console.ReadLine());
+double bathroomH = double.Parse(Console.ReadLine());
+double tileW = double.Parse(Console.ReadLine());
+double tileH = double.Parse(Console.ReadLine());
+double tilePersentage = 1.1;
+
+double bathroomArea = bathroomW * bathroomH;
+double bathroomSurplus = bathroomArea * tilePersentage;
+double tileArea = tileW * tileH;
+double tileNeeds = bathroomSurplus / tileArea;
+
+Console.WriteLine(Math.Round(tileNeeds));
