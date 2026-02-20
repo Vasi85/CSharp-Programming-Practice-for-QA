b711df8d0fad0c6454f7ca29e824f8421216941d
Vasi85 <101883373+Vasi85@users.noreply.github.com>
2026-02-20 15:09:21 +0200

    Add files via upload

---
+++ 01-Programming Fundamentals and Unit Testing/01-Exercises/06-WhileLoop/06NumberInRange/Program.cs
@@ -0,0 +1,8 @@
+﻿
+int num = int.Parse(Console.ReadLine());
+
+while (num < 1 || num >= 101)
+{ 
+     num = int.Parse(Console.ReadLine());
+}
+    Console.WriteLine(num);
---
+++ 01-Programming Fundamentals and Unit Testing/01-Exercises/06-WhileLoop/07NumberProcessor/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Data;
+
+int num = int.Parse(Console.ReadLine());
+string command = Console.ReadLine();
+
+while (command !="End")
+{
+    if (command == "Inc")
+    { 
+        ++num;
+        command = Console.ReadLine();
+    }
+    else if (command == "Dec")
+    {
+        --num;
+        command = Console.ReadLine();
+    }
+}
+Console.WriteLine(num);
