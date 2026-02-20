877ffd4cc29b3aec6a71016cd1323d316ead373f
Vasi85 <101883373+Vasi85@users.noreply.github.com>
2026-02-20 15:14:25 +0200

    Add files via upload

---
+++ 01-Programming Fundamentals and Unit Testing/01-Exercises/08-Methods/04CalculateRectangleArea/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _04CalculateRectangleArea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+
+            CalculateRectangleArea(width, height);
+
+            static void CalculateRectangleArea(int width, int height)
+            {
+                int area = width * height;
+                Console.WriteLine(area);
+            }
+            
+        }
+    }
+}
---
+++ 01-Programming Fundamentals and Unit Testing/01-Exercises/08-Methods/05RepeatString/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _05RepeatString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            int repeatCount = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(RepeatString(text, repeatCount));
+
+            static string RepeatString(string text, int repeatCount)
+            {
+                string repeat = "";
+
+                for (int i = 0; i < repeatCount; i++)
+                {
+                    repeat += text;
+                }
+                return repeat;
+            }
+        
+        }
+    }
+}
