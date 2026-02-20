bb004b426768d248cfc15edccb7063601472b25a
Vasi85 <101883373+Vasi85@users.noreply.github.com>
2026-02-20 15:08:32 +0200

    Add files via upload

---
+++ 01-Programming Fundamentals and Unit Testing/01-Exercises/06-WhileLoop/04Sequence2k+1/Program.cs
@@ -0,0 +1,9 @@
+﻿
+int currentnum = int.Parse(Console.ReadLine());
+int num = 1;
+
+while (num <= currentnum)
+{
+    Console.WriteLine(num);
+    num = num * 2 + 1;
+}
---
+++ 01-Programming Fundamentals and Unit Testing/01-Exercises/06-WhileLoop/05SumDigits/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using System.Diagnostics.CodeAnalysis;
+
+int currentNum = int.Parse(Console.ReadLine());
+int num = 1;
+int sum = 0;
+
+while (currentNum > 0)
+{
+    num = currentNum % 10;
+    sum += num;
+    currentNum = currentNum / 10;
+}
+Console.WriteLine(sum);
