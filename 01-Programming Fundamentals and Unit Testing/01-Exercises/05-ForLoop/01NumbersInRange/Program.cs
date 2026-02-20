1e1f073963947617cc9c299b4475dd034a5c7376
Vasi85 <101883373+Vasi85@users.noreply.github.com>
2026-02-20 15:04:14 +0200

    Add files via upload

---
+++ 01-Programming Fundamentals and Unit Testing/01-Exercises/05-ForLoop/01NumbersInRange/Program.cs
@@ -0,0 +1,8 @@
+﻿
+int startnum = int.Parse(Console.ReadLine());
+int endnum = int.Parse(Console.ReadLine());
+
+for (int i = startnum; startnum <= endnum; startnum++)
+{
+    Console.WriteLine(startnum);
+}
---
+++ 01-Programming Fundamentals and Unit Testing/01-Exercises/05-ForLoop/02FirstNNumbersSum/Program.cs
@@ -0,0 +1,18 @@
+﻿
+int n = int.Parse(Console.ReadLine());
+int sum = 0;
+
+for (int num = 1; num <= n; num++)
+{
+    
+    Console.Write(num);
+     sum += num;
+
+    if (num != n)
+    {
+        Console.Write("+");
+    }
+    else { 
+    Console.Write($"={sum}");
+    }
+}
