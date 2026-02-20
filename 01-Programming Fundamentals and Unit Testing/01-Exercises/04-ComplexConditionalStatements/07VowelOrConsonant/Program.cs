ed8b6b681b9149edbffd78bdf15c299a347620cc
Vasi85 <101883373+Vasi85@users.noreply.github.com>
2026-02-20 15:02:08 +0200

    Add files via upload

---
+++ 01-Programming Fundamentals and Unit Testing/01-Exercises/04-ComplexConditionalStatements/07VowelOrConsonant/Program.cs
@@ -0,0 +1,50 @@
+﻿
+char letter = char.Parse(Console.ReadLine());
+
+switch (letter)
+{
+    case 'a':
+        Console.WriteLine("Vowel");
+        break;
+
+    case 'A':
+        Console.WriteLine("Vowel");
+        break;
+
+    case 'e':
+        Console.WriteLine("Vowel");
+        break;
+
+    case 'E':
+        Console.WriteLine("Vowel");
+        break;
+
+    case 'i':
+        Console.WriteLine("Vowel");
+        break;
+
+    case 'I':
+        Console.WriteLine("Vowel");
+        break;
+
+    case 'o':
+        Console.WriteLine("Vowel");
+        break;
+
+    case 'O':
+        Console.WriteLine("Vowel");
+        break;
+
+    case 'u':
+        Console.WriteLine("Vowel");
+        break;
+
+    case 'U':
+        Console.WriteLine("Vowel");
+        break;
+
+    default:
+        Console.WriteLine("Consonant");
+        break;
+}
+
