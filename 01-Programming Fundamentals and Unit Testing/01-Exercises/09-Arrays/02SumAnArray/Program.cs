8a4a895c6cda7315de340bc09b10a4d80860d702
Vasi85 <101883373+Vasi85@users.noreply.github.com>
2026-02-20 15:19:35 +0200

    Add files via upload

---
+++ 01-Programming Fundamentals and Unit Testing/01-Exercises/09-Arrays/02SumAnArray/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace _02SumAnArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine()
+                                   .Split(' ')
+                                   .Select(int.Parse)
+                                   .ToArray();
+            int sum = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int currentNum = nums[i];
+                sum += currentNum;
+            }
+            Console.WriteLine(sum);
+          }
+      }
+  }
+
+            /*{
+            
+                {
+                    // Read input and split it into an array of strings
+                    string[] input = Console.ReadLine().Split(' ');
+
+                    // Convert and sum the numbers using a for loop
+                    int sum = 0;
+                    for (int i = 0; i < input.Length; i++)
+                    {
+                        sum += int.Parse(input[i]); // Convert each string to an integer and add to sum
+                    }
+
+                    // Print the result
+                    Console.WriteLine(sum);
+                }
+            }
+        }
+    }
+}*/
+
