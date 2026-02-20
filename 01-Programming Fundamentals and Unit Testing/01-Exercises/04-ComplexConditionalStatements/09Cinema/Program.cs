4f6cabe4d91f6ae35d0b58af4dcf9e6bd6d77758
Vasi85 <101883373+Vasi85@users.noreply.github.com>
2026-02-20 15:03:04 +0200

    Add files via upload

---
+++ 01-Programming Fundamentals and Unit Testing/01-Exercises/04-ComplexConditionalStatements/09Cinema/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System.Xml.Schema;
+
+string movieType = Console.ReadLine();
+int row = int.Parse(Console.ReadLine());
+int seats = int.Parse(Console.ReadLine());
+double premierTicket = 12.00;
+double normalTicket = 7.50;
+double discountTicket = 5.00;
+
+int countOfTickets = row * seats;
+
+switch (movieType)
+{
+    case "Premiere":
+        double totalPremierPrice = countOfTickets * premierTicket;
+        Console.WriteLine($"{totalPremierPrice:F2}");
+        break;
+
+    case "Normal":
+        double totalNormalPrice = countOfTickets * normalTicket;
+        Console.WriteLine($"{totalNormalPrice:F2}");
+        break;
+
+    case "Discount":
+        double totalDiscountPrice = countOfTickets * discountTicket;
+        Console.WriteLine($"{totalDiscountPrice:F2}");
+        break;
+
+    default:
+        break;
+}
