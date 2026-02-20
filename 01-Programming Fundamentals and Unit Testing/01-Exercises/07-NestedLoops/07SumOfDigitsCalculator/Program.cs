5d303f9a769688a3f534e10d666d195e9683a86c
Vasi85 <101883373+Vasi85@users.noreply.github.com>
2026-02-20 15:12:28 +0200

    Add files via upload

---
+++ 01-Programming Fundamentals and Unit Testing/01-Exercises/07-NestedLoops/07SumOfDigitsCalculator/Program.cs
@@ -0,0 +1,19 @@
+﻿
+string enter = Console.ReadLine();
+
+while (enter != "End")
+{
+    int num = int.Parse(enter);
+    int sumOfDigits = 0;
+
+    while (num > 0)
+    {
+        int digits = num % 10;
+        sumOfDigits += digits;
+        num /= 10;
+    }
+    Console.WriteLine($"Sum of digits = {sumOfDigits}");
+
+    enter = Console.ReadLine();
+}
+Console.WriteLine("Goodbye");
---
+++ 01-Programming Fundamentals and Unit Testing/01-Exercises/07-NestedLoops/08PrimeNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿
+int start = int.Parse(Console.ReadLine());
+int end = int.Parse(Console.ReadLine());
+
+for (int num = start; num <= end; num++)
+{
+    int primeNum = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        if (num % i == 0)
+        {
+            primeNum++;
+        }
+    }
+    if (primeNum == 2)
+    {
+        Console.Write($"{num} ");
+    }
+}
