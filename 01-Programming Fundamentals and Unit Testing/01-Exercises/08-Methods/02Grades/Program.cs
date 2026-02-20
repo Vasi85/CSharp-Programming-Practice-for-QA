f35432077c6d6f57d62aa5df835b76d54273fce9
Vasi85 <101883373+Vasi85@users.noreply.github.com>
2026-02-20 15:13:39 +0200

    Add files via upload

---
+++ 01-Programming Fundamentals and Unit Testing/01-Exercises/08-Methods/02Grades/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _02Grades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num = double.Parse(Console.ReadLine());
+            PrintGradesWord(num);
+
+
+            static void PrintGradesWord(double num)
+            {
+                string gradeWord = "";
+
+                switch (num)
+                {
+                    case >= 2.00 and <= 2.99:
+                        gradeWord = "Fail";
+                        break;
+                    case >= 3.00 and <= 3.49:
+                        gradeWord = "Average";
+                        break;
+                    case >= 3.50 and <= 4.49:
+                        gradeWord = "Good";
+                        break;
+                    case >= 4.50 and <= 5.49:
+                        gradeWord = "Very good";
+                        break;
+                    case >= 5.50 and <= 6.00:
+                        gradeWord = "Excellent";
+                        break;
+                }
+                Console.WriteLine(gradeWord);
+            }
+        
+
+        }
+    }
+}
---
+++ 01-Programming Fundamentals and Unit Testing/01-Exercises/08-Methods/03PrintingTriangle/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _03PrintingTriangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            PrintingTriangle(num);
+
+            static void PrintingTriangle(int num)
+            {
+                for (int i = 1; i < num; i++)
+                {
+                    LinePrinting(1, i);
+                }
+                for (int j = num; j >= 1; j--)
+                {
+                    LinePrinting(1, j);
+                }
+        
+            static void LinePrinting(int start, int end)
+                {
+                    for (int i = start; i <= end; i++)
+                    {
+                        Console.Write($"{i} ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
