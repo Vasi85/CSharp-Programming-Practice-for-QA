10cdd5e56b93635202d63719d23bc38523f9d0c5
Vasi85 <101883373+Vasi85@users.noreply.github.com>
2026-02-21 20:55:04 +0200

    Add files via upload

---
+++ 02-Programming Advanced for QA/01-Exercises/DictionariesLambdaAndLINQ/03WordSynonyms/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _03WordSynonyms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<string, List<string>> words = new Dictionary<string, List<string>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string word = Console.ReadLine();
+                string symonym = Console.ReadLine();
+
+                if (!words.ContainsKey(word))
+                {
+                    words.Add(word, new List<string>());
+                }
+                words[word].Add(symonym);
+            }
+            foreach (var item in words)
+            {
+                Console.WriteLine($"{item.Key} - {string.Join(", ", item.Value)}");
+            }
+        }
+    }
+}
