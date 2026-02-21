9b60c479d073fa06a8405ebee426bd88559e892d
Vasi85 <101883373+Vasi85@users.noreply.github.com>
2026-02-21 18:17:23 +0200

    Add files via upload

---
+++ 01-Programming Fundamentals and Unit Testing/02-Homeworks/02-HomeworkConditionalStatements/04NumberOperations/Program.cs
@@ -0,0 +1,30 @@
+﻿
+double num1 = double.Parse(Console.ReadLine());
+double num2 = double.Parse(Console.ReadLine());
+string @operator = Console.ReadLine();
+
+switch (@operator)
+{
+    case "+":
+        double addition = num1 + num2;
+        Console.WriteLine($"{num1} {@operator} {num2} = {addition:F2}");
+        break;
+    
+    case "-":
+        double subtraction = num1 - num2;
+        Console.WriteLine($"{num1} {@operator} {num2} = {subtraction:F2}");
+        break;
+
+    case "*":
+        double multiplication = num1 * num2;
+        Console.WriteLine($"{num1} {@operator} {num2} = {multiplication:F2}");
+        break;
+
+    case "/":
+        double division = num1 / num2;
+        Console.WriteLine($"{num1} {@operator} {num2} = {division:F2}");
+        break;
+
+    default:
+        break;
+}
---
+++ 01-Programming Fundamentals and Unit Testing/02-Homeworks/02-HomeworkConditionalStatements/05VacationExpenses/Program.cs
@@ -0,0 +1,82 @@
+﻿
+using System.ComponentModel.Design;
+using System.Xml.Schema;
+
+string season = Console.ReadLine();
+string place = Console.ReadLine();
+int days = int.Parse(Console.ReadLine());
+
+double springHotelPricePerDay = 30;
+double springCampingPricePerDay = 10;
+double summerHotelPricePerDay = 50;
+double summerCampingPricePerDay = 30;
+double autumnHotelPricePerDay = 20;
+double autumnCampingPricePerDay = 15;
+double winterHotelPricePerDay = 40;
+double winterCampingPricePerDay = 10;
+double discount = 1;
+
+if (season == "Spring")
+{
+    discount = 0.8;
+    {
+        if (place == "Hotel")
+        {
+            double totalSpringHotelPrice = springHotelPricePerDay * days * discount;
+            Console.WriteLine($"{totalSpringHotelPrice:F2}");
+        }
+        else if (place == "Camping")
+        {
+            double totalSpringCampingPrice = springCampingPricePerDay * days * discount;
+            Console.WriteLine($"{totalSpringCampingPrice:F2}");
+        }
+    }
+
+}
+else if (season == "Summer")
+    {
+    if (place == "Hotel")
+    {
+        double totalSummerHotelPrice = summerHotelPricePerDay * days;
+        Console.WriteLine($"{totalSummerHotelPrice:F2}");
+    }
+    else if (place == "Camping")
+    {
+        double totalSummerCampingPrice = summerCampingPricePerDay * days;
+        Console.WriteLine($"{totalSummerCampingPrice:F2}");
+    }
+}
+else if (season == "Autumn")
+{
+    discount = 0.7;
+    {
+        if (place == "Hotel")
+        {
+            double totalAutumnHotelPrice = autumnHotelPricePerDay * days * discount;
+            Console.WriteLine($"{totalAutumnHotelPrice:F2}");
+        }
+        else if (place == "Camping")
+        {
+            double totalAutumnCampingPrice = autumnCampingPricePerDay * days * discount;
+            Console.WriteLine($"{totalAutumnCampingPrice:F2}");
+        }
+    }
+    
+}
+else if (season == "Winter")
+{
+    discount = 0.9;
+    {
+        if (place == "Hotel")
+        {
+            double totalWinterHotelPrice = winterHotelPricePerDay * days * discount;
+            Console.WriteLine($"{totalWinterHotelPrice:F2}");
+        }
+        else if (place == "Camping")
+        {
+            double totalWinterCampingPrice = winterCampingPricePerDay * days * discount;
+            Console.WriteLine($"{totalWinterCampingPrice:F2}");
+        }
+    }
+
+}
