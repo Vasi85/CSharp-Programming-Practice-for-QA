31831bea6ccdc9320a0f6410fab87833a24b3935
Vasi85 <101883373+Vasi85@users.noreply.github.com>
2026-02-21 21:05:02 +0200

    Add files via upload

---
+++ 02-Programming Advanced for QA/01-Exercises/StringsndTextProcessing/03Substring/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _03Substring
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string first = Console.ReadLine();
+            string second = Console.ReadLine();
+
+            while (second.Contains(first))
+            {
+                int startIndex = second.IndexOf(first);
+
+                second = second.Remove(startIndex, first.Length); 
+            }
+            Console.WriteLine(second);
+        }
+    }
+}
