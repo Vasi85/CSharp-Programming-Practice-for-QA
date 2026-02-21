39af828b6243480535bf7bd0281c44c7e6e56add
Vasi85 <101883373+Vasi85@users.noreply.github.com>
2026-02-21 21:07:21 +0200

    Add files via upload

---
+++ 02-Programming Advanced for QA/02-Homeworks/Homework-DictionariesLambdaAndLINQ/01CountCharsInAString/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq.Expressions;
+
+namespace _01CountCharsInAString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Dictionary<char, int> characters = new Dictionary<char, int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char currentChar = input[i];
+
+                if (currentChar == ' ')
+                {
+                    continue;
+                }
+                if (!characters.ContainsKey(currentChar))
+                {
+                    characters.Add(currentChar, 1);
+                }
+                else
+                {
+                    characters[currentChar]++;
+                }
+            }
+
+            foreach (KeyValuePair<char, int> sym in characters)
+            {
+                Console.WriteLine($"{sym.Key} -> {sym.Value}");
+            }
+        }
+    }
+}
