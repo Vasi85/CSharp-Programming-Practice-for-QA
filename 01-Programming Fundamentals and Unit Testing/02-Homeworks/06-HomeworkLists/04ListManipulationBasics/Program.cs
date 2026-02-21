4f1c059b601c3da41bacd1d9f6cd913efb5df4ec
Vasi85 <101883373+Vasi85@users.noreply.github.com>
2026-02-21 18:31:38 +0200

    Add files via upload

---
+++ 01-Programming Fundamentals and Unit Testing/02-Homeworks/06-HomeworkLists/04ListManipulationBasics/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+namespace _04ListManipulationBasics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numberList = Console.ReadLine()
+                                          .Split(" ")
+                                          .Select(int.Parse)
+                                          .ToList();
+            string command = Console.ReadLine();
+
+            while (command != "end")
+            {
+                string[] commandNum = command.Split(" ");
+                string commandAction = commandNum[0];
+
+                if (commandAction == "Add")
+                {
+                    int numberToAdd = int.Parse(commandNum[1]);
+                    numberList.Add(numberToAdd);
+                }
+                else if (commandAction == "Insert")
+                {
+                    int number = int.Parse(commandNum[1]);
+                    int index = int.Parse(commandNum[2]);
+
+                    numberList.Insert(index, number);
+                }
+                else if (commandAction == "Remove")
+                {
+                    int number = int.Parse(commandNum[1]);
+                    numberList.Remove(number);
+                }
+                else if (commandAction == "RemoveAt")
+                {
+                    int index = int.Parse(commandNum[1]);
+                    numberList.RemoveAt(index);
+                }
+                command = Console.ReadLine();
+            }
+            Console.WriteLine(string.Join(" ", numberList));
+        }
+    }
+}
---
+++ 01-Programming Fundamentals and Unit Testing/02-Homeworks/06-HomeworkLists/05BombNumbers/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _05BombNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                                        .Split(" ")
+                                        .Select(int.Parse)
+                                        .ToList();
+            List<int> specialBombNum = Console.ReadLine()
+                                              .Split(" ")
+                                              .Select(int.Parse)
+                                              .ToList();
+
+            int bombNum = specialBombNum[0];
+            int bombPower = specialBombNum[1];
+
+            while (numbers.Contains(bombNum))
+            {
+                int bombNumIndex = numbers.IndexOf(bombNum);
+
+                int leftSide = bombNumIndex - bombPower;
+                if (leftSide < 0)
+                {
+                    leftSide = 0;
+                }
+
+                int rightSide = bombNumIndex + bombPower;
+                if (rightSide >= numbers.Count)
+                {
+                    rightSide = numbers.Count - 1;
+                }
+
+                numbers.RemoveRange(leftSide, rightSide - leftSide + 1);
+
+            }
+            Console.WriteLine(numbers.Sum());
+        }
+    }
+}
