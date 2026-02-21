14423e0ff87fa839fd01a2f850db7611d4dbcd41
Vasi85 <101883373+Vasi85@users.noreply.github.com>
2026-02-21 18:30:25 +0200

    Add files via upload

---
+++ 01-Programming Fundamentals and Unit Testing/02-Homeworks/06-HomeworkLists/02HouseParty/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace _02HouseParty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         int numOfGuestsOnList = int.Parse(Console.ReadLine());
+         List<string> guests = new List<string>();
+
+            for (int i = 0; i < numOfGuestsOnList; i++)
+            {
+                string command = Console.ReadLine();
+                string[] commandAction = command.Split(" ");
+
+                string name = commandAction[0];
+
+                if (command.Contains ("is going!"))
+                {  if (guests.Contains(name))
+                    {
+                        Console.WriteLine($"{name} is already in the list!");
+                    }
+                    else
+                    {
+                        guests.Add(name);
+                    }
+                }
+                else if (command.Contains("is not going!"))
+                {  if (guests.Contains(name))
+                    {
+                        guests.Remove(name);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{name} is not in the list!");
+                    }
+                    
+                }
+            }
+            
+            foreach (var guest in guests)
+            {
+                Console.WriteLine(guest);
+            }
+
+        }
+    }
+}
