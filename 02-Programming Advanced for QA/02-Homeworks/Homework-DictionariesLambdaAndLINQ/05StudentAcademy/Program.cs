a6cf3c09363c6aec4d6b1143eba87d52d2da831b
Vasi85 <101883373+Vasi85@users.noreply.github.com>
2026-02-21 21:09:36 +0200

    Add files via upload

---
+++ 02-Programming Advanced for QA/02-Homeworks/Homework-DictionariesLambdaAndLINQ/05StudentAcademy/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+using System.Xml.Linq;
+
+namespace _05StudentAcademy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<double>> studentsAverageGrade = new Dictionary<string, List<double>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string studentName = Console.ReadLine();
+                double grade = double.Parse(Console.ReadLine());
+
+                if (!studentsAverageGrade.ContainsKey(studentName))
+                {
+                    studentsAverageGrade.Add(studentName, new List<double>());
+                }
+                studentsAverageGrade[studentName].Add(grade);
+            }
+            foreach (var item in studentsAverageGrade.Where(s => s.Value.Average() >= 4.50))
+            {
+                string studentName = item.Key;
+                double averageGrade = item.Value.Average();
+
+                Console.WriteLine($"{item.Key} -> {averageGrade:F2}");
+            }
+        }
+    }
+}
