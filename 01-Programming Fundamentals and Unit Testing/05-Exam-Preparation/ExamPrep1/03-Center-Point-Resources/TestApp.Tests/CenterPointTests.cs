2419cace324415bda8c81683b4ea5fcdf5206c89
Vasi85 <101883373+Vasi85@users.noreply.github.com>
2026-02-21 19:54:23 +0200

    Add files via upload

---
+++ 01-Programming Fundamentals and Unit Testing/05-Exam-Preparation/ExamPrep1/03-Center-Point-Resources/TestApp.Tests/CenterPointTests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TestApp.Tests;
+
+public class CenterPointTests
+{
+    [Test]
+    public void Test_GetClosest_WhenFirstPointIsCloser_ShouldReturnFirstPoint()
+    {
+        //Arrange
+        string expected = "(1, 1)";
+
+        //Act
+        var actual = CenterPoint.GetClosest( 1, 1, 2, 2 );
+
+        //Assert
+        CollectionAssert.AreEqual(actual, expected);
+    }
+
+    [Test]
+    public void Test_GetClosest_WhenSecondPointIsCloser_ShouldReturnSecondPoint()
+    {
+        string expected = "(1, 1)";
+
+        //Act
+        var actual = CenterPoint.GetClosest(2, 2, 1, 1);
+
+        //Assert
+        CollectionAssert.AreEqual(actual, expected);
+    }
+
+    [Test]
+    public void Test_GetClosest_WhenBothPointsHaveEqualDistance_ShouldReturnFirstPoint()
+    {
+        string expected = "(2, 2)";
+
+        //Act
+        var actual = CenterPoint.GetClosest(2, 2, 2, 2);
+
+        //Assert
+        CollectionAssert.AreEqual(actual, expected);
+    }
+
+    [Test]
+    public void Test_GetClosest_WhenFirstPointIsNegative_ShouldReturnFirstPoint()
+    {
+        string expected = "(-2, -2)";
+
+        //Act
+        var actual = CenterPoint.GetClosest(-2, -2, 2, 2);
+
+        //Assert
+        CollectionAssert.AreEqual(actual, expected);
+    }
+
+    [Test]
+    public void Test_GetClosest_WhenSecondPointIsNegative_ShouldReturnSecondPoint()
+    {
+        string expected = "(-2, -2)";
+
+        //Act
+        var actual = CenterPoint.GetClosest(2, 2, -2, -2);
+
+        //Assert
+        CollectionAssert.AreEqual(actual, expected);
+    }
+}
---
+++ 01-Programming Fundamentals and Unit Testing/05-Exam-Preparation/ExamPrep1/04-FoldArray-Resources/TestApp.Tests/FoldSumTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TestApp.Tests;
+
+public class FoldSumTests
+{
+    [TestCase(new[] { 1, 2, 3, 4, 5, 6, 7, 8 }, "5 5 13 13")]
+    [TestCase(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 }, "7 7 7 19 19 19")]
+    [TestCase(new[] { 1, 2, -3, -4, 5, -6, 7, 8 }, "-1 -3 13 1")]
+    [TestCase(new[] { -1, -2, -3, -4, -5, -6, -7, -8 }, "-5 -5 -13 -13")]
+    [TestCase(new[] { 1, 1, 1, 1 }, "2 2")]
+    public void Test_FoldArray_ReturnsCorrectString(int[] arr, string expected)
+    {
+        string result = FoldSum.FoldArray(arr);
+
+        Assert.That(result, Is.EqualTo(expected));
+    }
+}
