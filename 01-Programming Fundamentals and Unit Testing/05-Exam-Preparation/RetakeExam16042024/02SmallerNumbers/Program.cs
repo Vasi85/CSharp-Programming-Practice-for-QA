0d64dbbbe8558a3d2e766385433fcb2942e19480
Vasi85 <101883373+Vasi85@users.noreply.github.com>
2026-02-21 20:08:56 +0200

    Add files via upload

---
+++ 01-Programming Fundamentals and Unit Testing/05-Exam-Preparation/RetakeExam16042024/02SmallerNumbers/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _02SmallerNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                                   .Split(' ')
+                                   .Select(int.Parse)
+                                   .ToArray();
+
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+
+                if (numbers[i] < N)
+                {
+                    Console.Write(numbers[i] + " ");
+                }
+            }
+            
+        }
+    }
+}
