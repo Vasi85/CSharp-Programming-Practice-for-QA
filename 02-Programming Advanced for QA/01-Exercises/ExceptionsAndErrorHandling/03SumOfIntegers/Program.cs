364eac933d744a836fb29f35cc0c4e703c9c1e1a
Vasi85 <101883373+Vasi85@users.noreply.github.com>
2026-02-21 20:57:05 +0200

    Add files via upload

---
+++ 02-Programming Advanced for QA/01-Exercises/ExceptionsAndErrorHandling/03SumOfIntegers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Xml.Linq;
+
+namespace _03SumOfIntegers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            string[] input = Console.ReadLine().Split(" ");
+
+            foreach (var element in input)
+            {
+                try
+                {
+                    int currentElement = int.Parse(element);
+                    numbers.Add(currentElement);
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine($"The element '{element}' is in wrong format!");
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine($"The element '{element}' is out of range!");
+                }
+                finally
+                {
+                    Console.WriteLine($"Element '{element}' processed - current sum: {numbers.Sum()}");
+                }
+            }
+            Console.WriteLine($"The total sum of all integers is: {numbers.Sum()}");
+
+
+        }
+    }
+}
