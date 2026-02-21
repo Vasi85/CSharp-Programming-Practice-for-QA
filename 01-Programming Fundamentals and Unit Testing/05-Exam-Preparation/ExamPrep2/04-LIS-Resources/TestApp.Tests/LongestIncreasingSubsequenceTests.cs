829f98a35539985c72faff9a2af1d3bffc9b70f4
Vasi85 <101883373+Vasi85@users.noreply.github.com>
2026-02-21 20:03:55 +0200

    Add files via upload

---
+++ 01-Programming Fundamentals and Unit Testing/05-Exam-Preparation/ExamPrep2/04-LIS-Resources/TestApp.Tests/LongestIncreasingSubsequenceTests.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace TestApp.Tests;
+
+public class LongestIncreasingSubsequenceTests
+{
+    [Test]
+    public void Test_GetLis_NullArray_ThrowsArgumentNullException()
+    {
+        int[] lis = null;
+
+        Assert.That(() => LongestIncreasingSubsequence.GetLis(lis), Throws.ArgumentNullException);
+    }
+
+    [Test]
+    public void Test_GetLis_EmptyArray_ReturnsEmptyString()
+    {
+        int[] lis = Array.Empty<int>();
+
+        string result = LongestIncreasingSubsequence.GetLis(lis);
+
+        Assert.AreEqual(string.Empty, result);
+    }
+
+    [Test]
+    public void Test_GetLis_SingleElementArray_ReturnsElement()
+    {
+        int[] lis = new[] { 1 };
+
+        string result = LongestIncreasingSubsequence.GetLis(lis);
+
+        Assert.That(result, Is.EqualTo("1"));
+    }
+
+    [Test]
+    public void Test_GetLis_UnsortedArray_ReturnsLongestIncreasingSubsequence()
+    {
+        int[] lisUnsort = new[] { 1, 15, 7, 12, 13, 3 };
+
+        string result = LongestIncreasingSubsequence.GetLis(lisUnsort);
+         
+        Assert.That(result, Is.EqualTo("1 7 12 13"));
+    }
+
+    [Test]
+    public void Test_GetLis_SortedArray_ReturnsItself()
+    {
+        int[] lisSorted = new[] { 1, 3, 7, 12, 13, 15 };
+
+        string result = LongestIncreasingSubsequence.GetLis(lisSorted);
+
+        Assert.That(result, Is.EqualTo("1 3 7 12 13 15"));
+    }
+}
