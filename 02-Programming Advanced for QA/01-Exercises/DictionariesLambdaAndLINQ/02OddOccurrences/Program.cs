ed2d8397c7a0535114d9c56fe822741575ca7e1a
Vasi85 <101883373+Vasi85@users.noreply.github.com>
2026-02-21 20:54:33 +0200

    Add files via upload

---
+++ 02-Programming Advanced for QA/01-Exercises/DictionariesLambdaAndLINQ/02OddOccurrences/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _02OddOccurrences
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> input = Console.ReadLine().Split(' ').Select(i => i.ToLower()).ToList();
+            Dictionary<string, int> wordCollection = new Dictionary<string, int>();
+
+        for (int i = 0; i < input.Count; i++)
+            {
+                string keyString = input[i];
+
+                if (wordCollection.ContainsKey(keyString))
+                {
+                    wordCollection[keyString]++;
+                }
+                else
+                {
+                    wordCollection.Add(keyString, 1);
+                }
+            }
+        foreach (var item in wordCollection)
+            {
+                if (item.Value % 2 != 0)
+                {
+                    Console.Write(item.Key + " ");
+                }
+            }
+            
+        }
+    }
+}
