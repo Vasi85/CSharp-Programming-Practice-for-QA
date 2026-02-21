ea087ad8502474c100a74906b7c3d0d6cb642a54
Vasi85 <101883373+Vasi85@users.noreply.github.com>
2026-02-21 21:05:40 +0200

    Add files via upload

---
+++ 02-Programming Advanced for QA/01-Exercises/StringsndTextProcessing/04TextFilter/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _04TextFilter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] bannedWords = Console.ReadLine().Split(", ");
+            string text = Console.ReadLine();
+            
+            foreach (var stringToRemove in bannedWords)
+            {
+                if (text.Contains(stringToRemove))
+                {
+                    text = text.Replace(stringToRemove, new string('*', stringToRemove.Length));
+                }
+            }
+            Console.WriteLine(text);
+        }
+    }
+}
