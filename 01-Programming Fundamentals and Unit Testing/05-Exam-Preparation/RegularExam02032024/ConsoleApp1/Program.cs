413142e25acbeb5054650d504b193d9a0a277daf
Vasi85 <101883373+Vasi85@users.noreply.github.com>
2026-02-21 20:06:11 +0200

    Add files via upload

---
+++ 01-Programming Fundamentals and Unit Testing/05-Exam-Preparation/RegularExam02032024/ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                                   .Split(' ')
+                                   .Select(int.Parse)
+                                   .ToArray();
+
+            int startIndex = int.Parse(Console.ReadLine());
+
+            int endIndex = int.Parse(Console.ReadLine());
+
+            int maxNumber = int.MinValue;
+            int minNumber = int.MaxValue;
+
+            for (int i = startIndex; i <= endIndex; i++)
+            {
+                int currentNumber = numbers[i];
+
+                if (currentNumber > maxNumber)
+                {
+                    maxNumber = currentNumber;
+                }
+                if (currentNumber < minNumber)
+                {
+                    minNumber = currentNumber;
+                }
+            }
+
+            int sum = maxNumber + minNumber;
+            Console.WriteLine(sum.ToString());
+        }
+    }
+}
