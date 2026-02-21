bf06a6f14baf135962962f40670d54775f1c737e
Vasi85 <101883373+Vasi85@users.noreply.github.com>
2026-02-21 19:56:33 +0200

    Add files via upload

---
+++ 01-Programming Fundamentals and Unit Testing/05-Exam-Preparation/ExamPrep2/02MinMaxValues/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace _02MinMaxValues
+{
+    internal class Program
+    {
+       /* static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            // Read integer N
+            int N = int.Parse(Console.ReadLine());
+
+            // Extract the first N elements
+            int[] firstNElements = numbers.Take(N).ToArray();
+
+            // Find max and min in the first N elements
+            int maxNumber = firstNElements.Max();
+            int minNumber = firstNElements.Min();
+
+            // Print results
+            Console.WriteLine(maxNumber);
+            Console.WriteLine(minNumber);
+        }*/
+        static void Mains(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                                   .Split()
+                                   .Select(int.Parse)
+                                   .ToArray();
+
+            int N = int.Parse(Console.ReadLine());
+
+            int minNumber = int.MaxValue;
+            int maxNumber = int.MinValue;
+
+            for (int i = 0; i < N; i++)
+            {
+                int number = numbers[i];
+                if (number > minNumber)
+                {
+                    minNumber = number;
+                }
+                if (number < maxNumber)
+                {
+                    maxNumber = number;
+                }
+            }
+            Console.Write($"{maxNumber}, {minNumber}");
+            
+        }
+    }
+}
