95763cd6823b76abaf2d6084c570fad0a77fba5e
Vasi85 <101883373+Vasi85@users.noreply.github.com>
2026-02-21 18:14:45 +0200

    Add files via upload

---
+++ 01-Programming Fundamentals and Unit Testing/02-Homeworks/01-HomeworkDataTypesVariablesAndKeyConcepts/06Aquarium/06Aquarium/Program.cs
@@ -0,0 +1,13 @@
+﻿
+int aquariumL = int.Parse(Console.ReadLine());
+int aquariumW = int.Parse(Console.ReadLine());
+int aquariumH = int.Parse(Console.ReadLine());
+double occupiedSpacePercentage = double.Parse(Console.ReadLine());
+double oneCubicCmWater = 0.001;
+
+int aquariumVol = aquariumL * aquariumW * aquariumH;
+double occupiedSpace = aquariumVol * occupiedSpacePercentage / 100;
+double water = (aquariumVol - occupiedSpace) * oneCubicCmWater;
+
+Console.WriteLine($"{water:F2}");
+
