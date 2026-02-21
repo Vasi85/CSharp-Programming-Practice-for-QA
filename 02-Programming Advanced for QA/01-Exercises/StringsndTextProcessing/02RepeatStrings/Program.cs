5106379e23cfa2a2ccaf84d37a70396e138cd4bd
Vasi85 <101883373+Vasi85@users.noreply.github.com>
2026-02-21 21:04:36 +0200

    Add files via upload

---
+++ 02-Programming Advanced for QA/01-Exercises/StringsndTextProcessing/02RepeatStrings/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics.Tracing;
+
+namespace _02RepeatStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputArray = Console.ReadLine().Split(" ");
+
+            string concatenated = "";
+
+            foreach (string word in inputArray)
+            {
+                int repeat = word.Length;
+                for (int i = 1; i <= word.Length; i++)
+                {
+                    concatenated += word;
+                }
+            }
+            Console.WriteLine(concatenated);
+        }
+    }
+}
