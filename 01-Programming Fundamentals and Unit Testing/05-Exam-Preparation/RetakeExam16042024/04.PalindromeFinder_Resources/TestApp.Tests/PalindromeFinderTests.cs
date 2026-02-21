e50b13a09e09258793f0ffd72d99d3d3a1a99685
Vasi85 <101883373+Vasi85@users.noreply.github.com>
2026-02-21 20:10:44 +0200

    Add files via upload

---
+++ 01-Programming Fundamentals and Unit Testing/05-Exam-Preparation/RetakeExam16042024/04.PalindromeFinder_Resources/TestApp.Tests/PalindromeFinderTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace TestApp.Tests;
+public class PalindromeFinderTests
+{
+    [Test]
+    public void Test_GetPalindromes_NullWordsList_ReturnsEmptyString()
+    {
+        List<string> nullList = null;
+
+        string actual = PalindromeFinder.GetPalindromes(nullList);
+
+        Assert.That(actual, Is.Empty);
+    }
+
+    [Test]
+    public void Test_GetPalindromes_EmptyWordsList_ReturnsEmptyString()
+    {
+        List<string> emptyList = new();
+
+        string actual = PalindromeFinder.GetPalindromes(emptyList);
+
+        Assert.That(actual, Is.Empty);
+    }
+
+    [Test]
+    public void Test_GetPalindromes_ListWithWords_ReturnsOnlyPalidromeWords()
+    {
+        List<string> listWithWords = new() { "cat", "dad", "Level", "Mom"};
+        string expected = "dad Level Mom";
+
+        string actual = PalindromeFinder.GetPalindromes(listWithWords);
+
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_GetPalindromes_ListWithoutPalindromeWords_ReturnsEmptyString()
+    {
+        List<string> listWithoutPalindromes = new() { "cat", "ball", "doll", "Mommy" };
+
+        string actual = PalindromeFinder.GetPalindromes(listWithoutPalindromes);
+
+        Assert.That(actual, Is.Empty);
+    }
+
+    [Test]
+    public void Test_GetPalindromes_ListOnlyWithPalidromeWords_ReturnsStringWithAllWords()
+    {
+        List<string> listWithWords = new() { "dad", "Level", "Mom" };
+        string expected = "dad Level Mom";
+
+        string actual = PalindromeFinder.GetPalindromes(listWithWords);
+
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+}
+
