8eb68db165b0bf18cd2e5c3af1f6288168495df0
Vasi85 <101883373+Vasi85@users.noreply.github.com>
2026-02-21 20:14:26 +0200

    Add files via upload

---
+++ 01-Programming Fundamentals and Unit Testing/05-Exam-Preparation/RetakeExam20122023/01CalculateSumPrimeNumbers/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _01CalculateSumPrimeNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+
+            for (int i = 0; i < N; i++)
+            {
+                int numValue = int.Parse(Console.ReadLine());
+                if (IsPrime(numValue))
+                {
+                    sum += numValue;
+
+                }
+
+            }
+            Console.WriteLine(sum);
+
+        }
+        static bool IsPrime(int num)
+        {
+            if (num == 0 || num == 1)
+            {
+                return false;
+            }
+
+            for (int i = 2; i < num; i++)
+            {
+                if (num % i ==0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
