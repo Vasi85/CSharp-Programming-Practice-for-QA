90cb3d14749bdb200178670587b76a655f6a8626
Vasi85 <101883373+Vasi85@users.noreply.github.com>
2026-02-21 20:08:23 +0200

    Add files via upload

---
+++ 01-Programming Fundamentals and Unit Testing/05-Exam-Preparation/RetakeExam16042024/01CalculateFactorialNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _01CalculateFactorialNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < N; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                Console.WriteLine(FactorialCalculate(number));
+            }
+        }
+
+        public static int FactorialCalculate(int number)
+        {
+            if (number == 0)
+            {
+                return 1;
+            }
+
+            return number * FactorialCalculate(number - 1);
+        }
+    }
+}
