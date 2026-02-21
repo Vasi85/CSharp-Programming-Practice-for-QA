929f1e462d953a839491b108dc227d831b9b148e
Vasi85 <101883373+Vasi85@users.noreply.github.com>
2026-02-21 20:12:04 +0200

    Add files via upload

---
+++ 01-Programming Fundamentals and Unit Testing/05-Exam-Preparation/RetakeExam17122024/02AverageSalesInSelectedRange/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _02AverageSalesInSelectedRange
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dailySalesAmounts = Console.ReadLine()
+                                             .Split(' ')
+                                             .Select(int.Parse)
+                                             .ToArray();
+
+            int startIndex = int.Parse(Console.ReadLine());
+            int endIndex = int.Parse(Console.ReadLine());
+            double sumSales = 0;
+
+            for (int i = startIndex; i <= endIndex; i++)
+            {
+                sumSales += dailySalesAmounts[i];
+            }
+
+            double average = sumSales / (endIndex - startIndex + 1);
+
+            Console.WriteLine($"{average:F2}");
+        }
+    }
+}
