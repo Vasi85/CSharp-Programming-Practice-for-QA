82e6cd5a02b121b9054d7a6d39bd3400e81408f2
Vasi85 <101883373+Vasi85@users.noreply.github.com>
2026-02-21 21:09:13 +0200

    Add files via upload

---
+++ 02-Programming Advanced for QA/02-Homeworks/Homework-DictionariesLambdaAndLINQ/04SoftUniParking/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace _04SoftUniParking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> parking = new Dictionary<string, string>();
+            
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] commands = Console.ReadLine().Split(' ');
+
+                string cmd = commands[0];
+                string userName = commands[1];
+
+                if (cmd == "register")
+                {
+                    string licensePlateNumber = commands[2];
+
+                    if (parking.ContainsKey(userName))
+                    {
+                        Console.WriteLine($"ERROR: already registered with plate number {licensePlateNumber}");
+                    }
+                    if (!parking.ContainsKey(userName))
+                    {
+                        parking.Add(userName, licensePlateNumber);
+                        Console.WriteLine($"{userName} registered {licensePlateNumber} successfully");
+                    }
+                }
+                else if (cmd == "unregister")
+                {
+                    if (!parking.ContainsKey(userName))
+                    {
+                        Console.WriteLine($"ERROR: user {userName} not found");
+                    }
+                    else
+                    {
+                        parking.Remove(userName);
+                        Console.WriteLine($"{userName} unregistered successfully");
+                    }
+                    
+                }
+            }
+            foreach (var item in parking)
+            {
+                Console.WriteLine($"{item.Key} => {item.Value}");
+            }
+        }
+    }
+}
