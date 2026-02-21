436c54b9a6a8a3e8ba03365efb598bc8d0d48fce
Vasi85 <101883373+Vasi85@users.noreply.github.com>
2026-02-21 18:16:14 +0200

    Add files via upload

---
+++ 01-Programming Fundamentals and Unit Testing/02-Homeworks/02-HomeworkConditionalStatements/02ATM/Program.cs
@@ -0,0 +1,17 @@
+﻿
+int balance = int.Parse(Console.ReadLine());
+int withdraw = int.Parse(Console.ReadLine());
+int limit = int.Parse(Console.ReadLine());
+
+if (withdraw <= balance && limit >= withdraw)
+{
+    Console.WriteLine("The withdraw was successful.");
+}
+else if (withdraw > balance &&  limit < withdraw)
+{
+    Console.WriteLine("The limit was exceeded.");
+}
+else if (withdraw > balance && limit > withdraw)
+{
+    Console.WriteLine("Insufficient availability.");
+}
