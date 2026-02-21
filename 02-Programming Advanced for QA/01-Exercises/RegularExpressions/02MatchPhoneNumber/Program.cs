3e61408cbdb5a8364c08611c5c60d84dfe85c0d0
Vasi85 <101883373+Vasi85@users.noreply.github.com>
2026-02-21 21:02:55 +0200

    Add files via upload

---
+++ 02-Programming Advanced for QA/01-Exercises/RegularExpressions/02MatchPhoneNumber/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _02MatchPhoneNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\+359([ -])2\1\d{3}\1\d{4}\b";
+            string phoneNum = Console.ReadLine();
+
+            MatchCollection number = Regex.Matches(phoneNum, pattern);
+
+            Console.WriteLine(string.Join(", ", number));
+        }
+    }
+}
