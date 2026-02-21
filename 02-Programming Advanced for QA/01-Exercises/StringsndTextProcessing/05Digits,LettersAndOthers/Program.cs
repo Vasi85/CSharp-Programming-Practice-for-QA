177e72d92a5310ce8adf22a23ecedebe4871c8fd
Vasi85 <101883373+Vasi85@users.noreply.github.com>
2026-02-21 21:06:06 +0200

    Add files via upload

---
+++ 02-Programming Advanced for QA/01-Exercises/StringsndTextProcessing/05Digits,LettersAndOthers/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _05Digits_LettersAndOthers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string digits = "";
+            string letters = "";
+            string charachers = "";
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (char.IsLetter(input[i]))
+                {
+                    letters += input[i];
+                }
+                else if (char.IsDigit(input[i]))
+                {
+                    digits += input[i];
+                }
+                else
+                {
+                    charachers += input[i];
+                }
+            }
+            Console.WriteLine(digits);
+            Console.WriteLine(letters);
+            Console.WriteLine(charachers);
+        }
+    }
+}
