563c4b3beec2ab5dd8e95c8b2aaa5aa3d0f129ef
Vasi85 <101883373+Vasi85@users.noreply.github.com>
2026-02-21 21:00:54 +0200

    Add files via upload

---
+++ 02-Programming Advanced for QA/01-Exercises/Objects and Classes/02Students/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace _02Students
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            List<Students> students = new List<Students>(); 
+
+            while (input != "end")
+            {
+                string[] studentInfo = input.Split(" ");
+                string firstName = studentInfo[0];
+                string lastName = studentInfo[1];
+                int age = int.Parse(studentInfo[2]);
+                string homeTown = studentInfo[3];
+
+                Students currentStudent = new Students(firstName, lastName, age, homeTown);
+
+                students.Add(currentStudent);
+
+                input = Console.ReadLine();
+
+            }
+            string town = Console.ReadLine();
+
+            foreach (Students student in students.Where(student => student.HomeTown == town))
+            {
+                Console.WriteLine($"{student.FirstName} {student.LastName} is {student.Age} years old.");
+
+            }
+        }
+    }
+    public class Students
+    {
+        public Students(string firstName, string lastName, int age, string homeTown)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            HomeTown = homeTown;
+        }
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Age { get; }
+
+        public string HomeTown { get; set; }
+    }
+}
