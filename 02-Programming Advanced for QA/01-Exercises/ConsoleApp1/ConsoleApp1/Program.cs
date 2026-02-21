4d64faf53c1e9db3c52395bb3b892335a6bffbf3
Vasi85 <101883373+Vasi85@users.noreply.github.com>
2026-02-21 20:49:46 +0200

    Add files via upload

---
+++ 02-Programming Advanced for QA/01-Exercises/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Ivan";
+            char[] chars = name.ToCharArray();
+            chars[3] = 'r';
+            string newName = new string(chars);
+            Console.WriteLine(newName);
+        }
+    }
+}
