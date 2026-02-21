6b75d6c9c625de4173f8c31e95f84277838558e7
Vasi85 <101883373+Vasi85@users.noreply.github.com>
2026-02-21 20:05:33 +0200

    Add files via upload

---
+++ 01-Programming Fundamentals and Unit Testing/05-Exam-Preparation/RegularExam02032024/01CountLetters/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _01CountLetters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        string input = Console.ReadLine();
+
+        int countLowerLetter = 0;
+        int countUpperLetter = 0;
+        int countSingleSpase = 0;
+
+            foreach (char c in input)
+            {
+                if (char.IsUpper(c))
+                {
+                    countUpperLetter++;
+                }
+                else if (char.IsLower(c))
+                {
+                    countLowerLetter++;
+                }
+                else if (char.IsWhiteSpace(c))
+                {
+                    countSingleSpase++;
+                }
+            }
+            Console.WriteLine(countUpperLetter);
+            Console.WriteLine(countLowerLetter);
+            Console.WriteLine(countSingleSpase);
+        }
+    }
+}
