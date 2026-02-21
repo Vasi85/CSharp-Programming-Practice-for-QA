908f619e4f970e61e458654a814136d407fb8f3c
Vasi85 <101883373+Vasi85@users.noreply.github.com>
2026-02-21 19:51:33 +0200

    Add files via upload

---
+++ 01-Programming Fundamentals and Unit Testing/05-Exam-Preparation/ExamPrep1/01SumFactorialEvenDigits/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _01SumFactorialEvenDigits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            int sumEvenDigits = 0;
+
+            while (num > 0)
+            {
+                int digit = num % 10;
+                if (digit %2  == 0)
+                {
+                    sumEvenDigits += Factorial(digit);
+                }
+                num /= 10;
+            }
+
+            Console.WriteLine(sumEvenDigits);
+        }
+
+        static int Factorial(int n) // n = 5
+        {
+            int result = 1;
+            if (n == 0 || n == 1)
+            {
+                return result;
+            }
+
+            for (int i = n; i >= 2; i--)
+            {
+                result *= i;
+            }
+
+            return result;
+        }
+    }
+}
