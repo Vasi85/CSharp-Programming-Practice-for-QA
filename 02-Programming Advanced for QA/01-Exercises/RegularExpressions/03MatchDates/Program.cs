0279609d551007b896ca603f1f740ad476c40add
Vasi85 <101883373+Vasi85@users.noreply.github.com>
2026-02-21 21:03:22 +0200

    Add files via upload

---
+++ 02-Programming Advanced for QA/01-Exercises/RegularExpressions/03MatchDates/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _03MatchDates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"(?<Day>\d{2})([./-])(?<Month>[A-Z][a-z]{2})\1(?<Year>\d{4})";
+            string dates = Console.ReadLine();
+
+            MatchCollection validDate = Regex.Matches(dates, pattern);
+
+            foreach (Match value in validDate)
+            {
+                string day = value.Groups["Day"].Value;
+                string month = value.Groups["Month"].Value;
+                string year = value.Groups["Year"].Value;
+
+                Console.WriteLine($"Day: {day}, Month: {month}, Year: {year}");
+            }
+        }
+    }
+}
