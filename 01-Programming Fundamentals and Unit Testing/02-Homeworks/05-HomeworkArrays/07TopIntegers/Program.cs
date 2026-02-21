682067f630c336274ab50892e917ba5b3b665488
Vasi85 <101883373+Vasi85@users.noreply.github.com>
2026-02-21 18:29:25 +0200

    Add files via upload

---
+++ 01-Programming Fundamentals and Unit Testing/02-Homeworks/05-HomeworkArrays/07TopIntegers/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _07TopIntegers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayNum = Console.ReadLine()
+                                    .Split(" ")
+                                    .Select(int.Parse)
+                                    .ToArray();
+            for (int i = 0; i < arrayNum.Length; i++)
+            {
+                int number = arrayNum[i];
+                bool IsTopNumber = true;
+
+                for (int j = i + 1; j < arrayNum.Length; j++)
+                {
+                    if (number <= arrayNum[j])
+                    {
+                        IsTopNumber = false;
+                        break;
+                    }
+                }
+                if (IsTopNumber)
+                {
+                    Console.Write($"{number} ");
+                } 
+            }
+        }
+    }
+}
