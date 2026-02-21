aa2b32e02c8b4d9bdc93a383a09b91474e9f884b
Vasi85 <101883373+Vasi85@users.noreply.github.com>
2026-02-21 20:56:11 +0200

    Add files via upload

---
+++ 02-Programming Advanced for QA/01-Exercises/ExceptionsAndErrorHandling/01SquareRoot/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _01SquareRoot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            try
+            {                
+                if(n < 0)
+                {
+                    throw new Exception("Invalid number.");
+                }
+
+                double result = Math.Sqrt(n);
+                Console.WriteLine(result);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye.");
+            }
+        }
+    }
+}
