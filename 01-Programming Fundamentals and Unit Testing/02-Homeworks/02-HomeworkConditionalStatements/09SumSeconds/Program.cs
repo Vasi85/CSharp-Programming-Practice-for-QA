1084e222db8e3a2559fd105d1e63c10149c90a3f
Vasi85 <101883373+Vasi85@users.noreply.github.com>
2026-02-21 18:19:31 +0200

    Add files via upload

---
+++ 01-Programming Fundamentals and Unit Testing/02-Homeworks/02-HomeworkConditionalStatements/09SumSeconds/Program.cs
@@ -0,0 +1,11 @@
+﻿
+int time1 = int.Parse(Console.ReadLine());
+int time2 = int.Parse(Console.ReadLine());
+int time3 = int.Parse(Console.ReadLine());
+
+int totalTime = time1 + time2 + time3;
+
+int totalTimeInMinutes = totalTime / 60;
+int totalTimeInSeconds = totalTime % 60;
+
+Console.WriteLine($"{totalTimeInMinutes}:{totalTimeInSeconds:D2}");
---
+++ 01-Programming Fundamentals and Unit Testing/02-Homeworks/02-HomeworkConditionalStatements/10SummerOutfit/Program.cs
@@ -0,0 +1,32 @@
+﻿
+int temperature = int.Parse(Console.ReadLine());
+string partOfDay = Console.ReadLine();
+
+string clothing = "Shirt";
+string shoes = "Moccasins";
+
+if ((temperature >= 10 && temperature <= 18 && partOfDay != "Morning") 
+    || (temperature >= 25 && partOfDay == "Evening")
+    || (temperature > 18 && temperature <=24 && partOfDay != "Afternoon"))
+{
+        Console.WriteLine($"It's {temperature} degrees, get your {clothing} and {shoes}.");
+}
+else if (temperature >= 10 && temperature <= 18 && partOfDay == "Morning")
+{
+    clothing = "Sweatshirt";
+    shoes = "Sneakers";
+    Console.WriteLine($"It's {temperature} degrees, get your {clothing} and {shoes}.");
+}
+else if ((temperature > 18 && temperature <= 24 && partOfDay == "Afternoon") 
+        || (temperature >= 25 && partOfDay == "Morning"))
+{
+    clothing = "T-Shirt";
+    shoes = "Sandals";
+    Console.WriteLine($"It's {temperature} degrees, get your {clothing} and {shoes}.");
+}
+else if (temperature >= 25 && partOfDay == "Afternoon")
+{
+    clothing = "Swim Suit";
+    shoes = "Barefoot";
+    Console.WriteLine($"It's {temperature} degrees, get your {clothing} and {shoes}.");
+}
