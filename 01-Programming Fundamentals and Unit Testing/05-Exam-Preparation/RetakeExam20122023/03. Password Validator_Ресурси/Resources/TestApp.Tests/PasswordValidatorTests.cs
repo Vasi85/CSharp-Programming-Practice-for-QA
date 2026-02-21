421ab9cb34f6173b04d857dae4b2f7f46d38e3aa
Vasi85 <101883373+Vasi85@users.noreply.github.com>
2026-02-21 20:16:29 +0200

    Add files via upload

---
+++ 01-Programming Fundamentals and Unit Testing/05-Exam-Preparation/RetakeExam20122023/03. Password Validator_Ресурси/Resources/TestApp.Tests/PasswordValidatorTests.cs
@@ -0,0 +1,61 @@
+﻿using NUnit.Framework;
+
+namespace TestApp.Tests;
+
+public class PasswordValidatorTests
+{
+    [Test]
+    public void Test_CheckPassword_ValidPassword_ReturnsValidMessage()
+    {
+        string validPass = "Pass123";
+        string expected = "Password is valid";
+
+        string actual = PasswordValidator.CheckPassword(validPass);
+
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_CheckPassword_PasswordTooShort_ReturnsErrorMessage()
+    {
+        string invalidPass = "Short";
+        string expected = "Password must be between 6 and 10 characters";
+
+        string actual = PasswordValidator.CheckPassword(invalidPass);
+
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_CheckPassword_ContainsSpecialCharacters_ReturnsErrorMessage()
+    {
+        string invalidPass = "Inv@Pass";
+        string expected = "Password must consist only of letters and digits";
+
+        string actual = PasswordValidator.CheckPassword(invalidPass);
+
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_CheckPassword_InsufficientDigits_ReturnsErrorMessage()
+    {
+        string empty = "NoDigits";
+        string expected = "Password must have at least 2 digits";
+
+        string actual = PasswordValidator.CheckPassword(empty);
+
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_CheckPassword_ValidPasswordWithMaximumLength_ReturnsValidMessage()
+    {
+        string validPassMaxLength = "ValPass123";
+        string expected = "Password is valid";
+
+        string actual = PasswordValidator.CheckPassword(validPassMaxLength);
+
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+}
