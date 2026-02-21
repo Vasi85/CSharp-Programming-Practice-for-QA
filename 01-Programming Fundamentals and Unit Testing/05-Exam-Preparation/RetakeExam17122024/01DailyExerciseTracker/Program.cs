782eb755f4b39d5dd77001d2f6603430f5623288
Vasi85 <101883373+Vasi85@users.noreply.github.com>
2026-02-21 20:11:17 +0200

    Add files via upload

---
+++ 01-Programming Fundamentals and Unit Testing/05-Exam-Preparation/RetakeExam17122024/01DailyExerciseTracker/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _01DailyExerciseTracker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+
+            if (days <= 0)
+            {
+                Console.WriteLine(0);
+            }
+
+            int cumulativeExerciseTime = 0;
+            for (int i = 1; i <= days; i++)
+            {
+                int minExercisePerDay = int.Parse(Console.ReadLine());
+                cumulativeExerciseTime += minExercisePerDay;
+                minExercisePerDay = cumulativeExerciseTime;
+                Console.WriteLine(cumulativeExerciseTime);
+                
+            }
+        }
+    }
+}
