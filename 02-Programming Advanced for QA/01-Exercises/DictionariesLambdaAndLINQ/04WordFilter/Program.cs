6906c160bfddbbaa2d07a9803519815ec1e60ae6
Vasi85 <101883373+Vasi85@users.noreply.github.com>
2026-02-21 20:55:32 +0200

    Add files via upload

---
+++ 02-Programming Advanced for QA/01-Exercises/DictionariesLambdaAndLINQ/04WordFilter/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _04WordFilter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine()
+                                    .Split(' ')
+                                    .Where(i => i.Length % 2 == 0)
+                                    .ToArray();
+
+            foreach (var word in input)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
