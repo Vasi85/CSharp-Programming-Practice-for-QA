0675266858905f59229e09f32929de929b13261d
Vasi85 <101883373+Vasi85@users.noreply.github.com>
2026-02-21 18:10:28 +0200

    Add files via upload

---
+++ 01-Programming Fundamentals and Unit Testing/01-Exercises/10-Lists/01ChangeList/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace _01ChangeList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         List<int> nums = Console.ReadLine()
+                                 .Split(" ")
+                                 .Select(int.Parse)
+                                 .ToList();
+            string command = Console.ReadLine();
+
+            while (command != "end")
+            {
+                string[] commandNum = command.Split(" ");
+                string commandAction = commandNum[0];
+
+                if (commandAction == "Delete")
+                {
+                    int numberToRemove = int.Parse(commandNum[1]);
+
+                    for (int i = 0; i < nums.Count; i++)
+                    {
+                        if (nums[i] == numberToRemove)
+                        {
+                            nums.RemoveAt(i);
+                            i--;
+                        }
+                    }
+                }
+                else if (commandAction == "Insert")
+                {
+                    int element = int.Parse(commandNum[1]);
+                    int position = int.Parse(commandNum[2]);
+
+                    nums.Insert(position, element);
+                }
+                command = Console.ReadLine();
+            }
+            Console.WriteLine(string.Join(" ", nums));
+        }
+    }
+}
---
+++ 01-Programming Fundamentals and Unit Testing/01-Exercises/10-Lists/03RemoveNegativesAndReverse/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _03RemoveNegativesAndReverse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         List<int> numsList = Console.ReadLine()
+                                     .Split(" ")
+                                     .Select(int.Parse)
+                                     .ToList();
+            for (int i = 0; i < numsList.Count; i++)
+            {
+                if (numsList[i] < 0)
+                {
+                    numsList.RemoveAt(i);
+                    i--;
+                }
+            }
+            numsList.Reverse();
+
+            if (numsList.Count != 0)
+            {
+                Console.WriteLine(string.Join(" ", numsList));
+            }
+            else
+            {
+                Console.WriteLine("empty");
+            }
+
+        }
+    }
+}
