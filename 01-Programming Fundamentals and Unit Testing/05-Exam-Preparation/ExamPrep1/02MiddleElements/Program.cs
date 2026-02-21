40ac79d9d527458b3678bdc7e3549c3fb447b0d1
Vasi85 <101883373+Vasi85@users.noreply.github.com>
2026-02-21 19:52:58 +0200

    Add files via upload

---
+++ 01-Programming Fundamentals and Unit Testing/05-Exam-Preparation/ExamPrep1/02MiddleElements/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _02MiddleElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numArray = Console.ReadLine()
+                                    .Split(" ")
+                                    .Select(int.Parse)
+                                    .ToArray();
+
+            int mid = numArray.Length / 2;
+            double result = 0;
+
+            for (int i = mid - 1; i <= mid; i++)
+            {
+                result += numArray[i];
+            }
+
+            double divisionMid = result / 2;
+
+            Console.WriteLine($"{divisionMid:F2}");
+        }
+    }
+}
