06aba05df0b9c5d8cc1f887520b544fa47924edd
Vasi85 <101883373+Vasi85@users.noreply.github.com>
2026-02-21 21:03:59 +0200

    Add files via upload

---
+++ 02-Programming Advanced for QA/01-Exercises/StringsndTextProcessing/01ReverseStrings/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _01ReverseStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string  word = Console.ReadLine();
+
+            while (word != "end")
+            {
+                string reversedWord = "";
+                for (int i = word.Length - 1; i >= 0; i--)
+                {
+                    reversedWord += word[i];
+                }
+                Console.WriteLine($"{word} = {reversedWord}");
+
+                word = Console.ReadLine();
+            }   
+        }
+    }
+}
