d51dccccfcfa53d4168226a889a4aee9b59f17d8
Vasi85 <101883373+Vasi85@users.noreply.github.com>
2026-02-21 19:42:59 +0200

    Add files via upload

---
+++ 01-Programming Fundamentals and Unit Testing/06-RegularExam2032025/01WaterConsumption/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _01WaterConsumption
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfDays = int.Parse(Console.ReadLine());
+            int cumulativeWater = 0;
+
+            if (countOfDays <= 0)
+            {
+                Console.WriteLine(0);
+                return;
+            }
+
+            for (int i = 0; i < countOfDays; i++)
+            {
+                int water = int.Parse(Console.ReadLine());
+
+                {
+                    cumulativeWater += water;
+                    Console.WriteLine(cumulativeWater);
+                }
+
+            }
+            
+        }
+    }
+}
---
+++ 01-Programming Fundamentals and Unit Testing/06-RegularExam2032025/02AverageAndTotalRainfall/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _02AverageAndTotalRainfall
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dailyRainfallAmounts = Console.ReadLine()
+                                                .Split(' ')
+                                                .Select(int.Parse)
+                                                .ToArray();
+
+            int startDay = int.Parse(Console.ReadLine());
+            int endDay = int.Parse(Console.ReadLine());
+            double total = 0;
+
+            for (int i = startDay; i <= endDay; i++)
+            {
+                total += dailyRainfallAmounts[i];
+            }
+            Console.WriteLine(total);
+
+            double averageRainfall = total / (endDay - startDay + 1);
+            Console.WriteLine($"{averageRainfall:F2}");
+        }
+    }
+}
