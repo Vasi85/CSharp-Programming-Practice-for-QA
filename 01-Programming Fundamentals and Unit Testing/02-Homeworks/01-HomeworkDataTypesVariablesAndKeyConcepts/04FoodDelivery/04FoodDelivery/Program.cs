a38b4081e34c35747993ed00c0e6aca683487d33
Vasi85 <101883373+Vasi85@users.noreply.github.com>
2026-02-21 18:14:03 +0200

    Add files via upload

---
+++ 01-Programming Fundamentals and Unit Testing/02-Homeworks/01-HomeworkDataTypesVariablesAndKeyConcepts/04FoodDelivery/04FoodDelivery/Program.cs
@@ -0,0 +1,20 @@
+﻿
+double chickenMenuPrice = 10.35;
+double fishMenuPrice = 12.40;
+double vegetarianMenuPrice = 8.15;
+double deliveryPrice = 2.50;
+double desertPricePercentage = 0.2;
+
+int chickenMenu = int.Parse(Console.ReadLine());
+int fishMenu = int.Parse(Console.ReadLine());
+int vegetarianMenu = int.Parse(Console.ReadLine());
+
+double chickenMenuCost = chickenMenu * chickenMenuPrice;
+double fishMenuCost = fishMenu * fishMenuPrice;
+double vegetarianMenuCost = vegetarianMenu * vegetarianMenuPrice;
+double totalCostOfMenus = chickenMenuCost + fishMenuCost + vegetarianMenuCost;
+double desertCost = totalCostOfMenus * desertPricePercentage;
+
+double totalOrderCost = totalCostOfMenus + desertCost + deliveryPrice;
+
+Console.WriteLine(totalOrderCost);
---
+++ 01-Programming Fundamentals and Unit Testing/02-Homeworks/01-HomeworkDataTypesVariablesAndKeyConcepts/05BasketballEquipment/05BasketballEquipment/Program.cs
@@ -0,0 +1,16 @@
+﻿
+double sneakersPrice = 0.6;
+double uniformPrice = 0.8;
+double ballPrice = 0.25;
+double accessoriesPrice = 0.2;
+
+int bascketballTrainingFee = int.Parse(Console.ReadLine());
+
+double sneacersCost = bascketballTrainingFee * sneakersPrice;
+double uniformCost = sneacersCost * uniformPrice;
+double ballCost = uniformCost * ballPrice;
+double accessoriesCost = ballCost * accessoriesPrice;
+
+double totalCost = bascketballTrainingFee + sneacersCost + uniformCost + ballCost + accessoriesCost;
+
+Console.WriteLine(totalCost);
