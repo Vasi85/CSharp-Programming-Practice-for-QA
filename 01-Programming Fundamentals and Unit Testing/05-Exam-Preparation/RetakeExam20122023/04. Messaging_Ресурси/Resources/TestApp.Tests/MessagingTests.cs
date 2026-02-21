b550f7931ce0501f4f2ca729a72040556a6acbe4
Vasi85 <101883373+Vasi85@users.noreply.github.com>
2026-02-21 20:16:58 +0200

    Add files via upload

---
+++ 01-Programming Fundamentals and Unit Testing/05-Exam-Preparation/RetakeExam20122023/04. Messaging_Ресурси/Resources/TestApp.Tests/MessagingTests.cs
@@ -0,0 +1,66 @@
+﻿using NUnit.Framework;
+
+using System.Collections.Generic;
+
+namespace TestApp.Tests;
+
+public class MessagingTests
+{
+    [Test]
+    public void Test_GetMessage_WithValidInput_ReturnsExpectedMessage()
+    {
+        List<int> validInput = new() { 1, 2, 3 };
+        string text = "Hello";
+
+        string expected = "elH";
+
+
+        string actual = Messaging.GetMessage(validInput, text);
+
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_GetMessage_EmptyList_ReturnsEmptyString()
+    {
+        List<int> emptyList = new() {};
+        string text = "Hello";
+
+        string actual = Messaging.GetMessage(emptyList, text);
+
+        Assert.That(actual, Is.Empty);
+    }
+
+    [Test]
+    public void Test_GetMessage_EmptyString_ReturnsEmptyString()
+    {
+        List<int> emptyList = new() { 1, 2, 3 };
+        string text = "";
+
+        string actual = Messaging.GetMessage(emptyList, text);
+
+        Assert.That(actual, Is.Empty);
+    }
+
+    [Test]
+    public void Test_GetMessage_NullList_ReturnsEmptyString()
+    {
+        List<int> emptyList = null;
+        string text = "Hi";
+
+        string actual = Messaging.GetMessage(emptyList, text);
+
+        Assert.That(actual, Is.Empty);
+    }
+
+    [Test]
+    public void Test_GetMessage_NullString_ReturnsEmptyString()
+    {
+        List<int> emptyList = new() { 1, 2, 3 };
+        string text = null;
+
+        string actual = Messaging.GetMessage(emptyList, text);
+
+        Assert.That(actual, Is.Empty);
+    }
+}
