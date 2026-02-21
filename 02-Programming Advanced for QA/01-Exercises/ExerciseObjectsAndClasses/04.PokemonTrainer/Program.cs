89bfda64eb0525ece0db6458921ada2b6b4ecb65
Vasi85 <101883373+Vasi85@users.noreply.github.com>
2026-02-21 20:59:16 +0200

    Add files via upload

---
+++ 02-Programming Advanced for QA/01-Exercises/ExerciseObjectsAndClasses/04.PokemonTrainer/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _04.PokemonTrainer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
