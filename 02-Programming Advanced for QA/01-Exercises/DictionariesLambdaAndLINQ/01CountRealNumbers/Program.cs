b335096db7c582c88b385df5545cea801de71b90
Vasi85 <101883373+Vasi85@users.noreply.github.com>
2026-02-21 20:54:00 +0200

    Add files via upload

---
+++ 02-Programming Advanced for QA/01-Exercises/DictionariesLambdaAndLINQ/01CountRealNumbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace _01CountRealNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            SortedDictionary<int, int> numbers = new SortedDictionary<int, int>();
+
+            foreach (int num in nums)
+            {
+                if (numbers.ContainsKey(num))
+                {
+                    numbers[num]++;
+                }
+                else
+                {
+                    numbers[num] = 1;
+                }
+            }
+            foreach (KeyValuePair<int, int> numPair in numbers)
+            {
+                Console.WriteLine($"{numPair.Key} -> {numPair.Value}");
+            }
+        }
+    }
+}
