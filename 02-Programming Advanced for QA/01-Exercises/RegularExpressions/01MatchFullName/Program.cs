1da24f3c22aeb9adf34512bdadb0cf05094afbda
Vasi85 <101883373+Vasi85@users.noreply.github.com>
2026-02-21 21:02:17 +0200

    Add files via upload

---
+++ 02-Programming Advanced for QA/01-Exercises/RegularExpressions/01MatchFullName/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _01MatchFullName
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string pattern = @"\b[A-Z][a-z]+ [A-Z][a-z]+\b";
+            string names = Console.ReadLine();
+
+            MatchCollection validFullNames = Regex.Matches(names, pattern);
+
+            foreach (Match name in validFullNames)
+            {
+                Console.Write($"{name.Value} ");
+            }
+        }
+    }
+}
