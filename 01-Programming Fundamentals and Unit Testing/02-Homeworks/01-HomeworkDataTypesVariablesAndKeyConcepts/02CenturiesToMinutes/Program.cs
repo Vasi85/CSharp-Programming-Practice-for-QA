91693732f7fe3da3e0f16da44d8ece6fc6896ec0
Vasi85 <101883373+Vasi85@users.noreply.github.com>
2026-02-21 18:12:34 +0200

    Add files via upload

---
+++ 01-Programming Fundamentals and Unit Testing/02-Homeworks/01-HomeworkDataTypesVariablesAndKeyConcepts/02CenturiesToMinutes/Program.cs
@@ -0,0 +1,9 @@
+﻿
+int centuries = int.Parse(Console.ReadLine());
+
+int years = centuries * 100;
+double days = Math.Round(years * 365.2422);
+double hours = days * 24;
+double minutes = hours * 60;
+
+Console.WriteLine($"{centuries} centuries = {years} years = {days:F0} days = {hours:F0} hours = {minutes:F0} minutes"); 
