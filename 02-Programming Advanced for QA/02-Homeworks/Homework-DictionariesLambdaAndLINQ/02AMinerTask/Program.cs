61d34e40cb9c237d8c7512ec0759630f4402d9c1
Vasi85 <101883373+Vasi85@users.noreply.github.com>
2026-02-21 21:08:33 +0200

    Add files via upload

---
+++ 02-Programming Advanced for QA/02-Homeworks/Homework-DictionariesLambdaAndLINQ/02AMinerTask/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _02AMinerTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string resource = Console.ReadLine();
+
+            Dictionary<string, int> resourcesCount = new Dictionary<string, int>();
+
+            while (resource != "stop")
+            {
+                int quantity = int.Parse(Console.ReadLine());
+
+                if (!resourcesCount.ContainsKey(resource))
+                {
+                    resourcesCount.Add(resource, 0);
+                }
+                resourcesCount[resource] += quantity;
+
+                resource = Console.ReadLine();
+            }
+
+            foreach (var item in resourcesCount)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
