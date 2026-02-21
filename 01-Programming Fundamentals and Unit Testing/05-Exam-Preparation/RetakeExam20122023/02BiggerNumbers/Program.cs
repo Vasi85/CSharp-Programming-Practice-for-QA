0c32e3ac80af959b034ec14b745ec902a973608c
Vasi85 <101883373+Vasi85@users.noreply.github.com>
2026-02-21 20:15:05 +0200

    Add files via upload

---
+++ 01-Programming Fundamentals and Unit Testing/05-Exam-Preparation/RetakeExam20122023/02BiggerNumbers/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _02BiggerNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                                   .Split(' ')
+                                   .Select(int.Parse)
+                                   .ToArray();
+
+            int N = int.Parse(Console.ReadLine());
+
+            foreach (int num in numbers)
+            {
+                if (num > N)
+                {
+                    Console.Write(num + " ");
+                }
+            }
+            
+        }
+    }
+}
