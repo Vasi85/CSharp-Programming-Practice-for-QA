ef43b26995b1693b43e59d7b55eb1fcb212520b7
Vasi85 <101883373+Vasi85@users.noreply.github.com>
2026-02-21 18:18:38 +0200

    Add files via upload

---
+++ 01-Programming Fundamentals and Unit Testing/02-Homeworks/02-HomeworkConditionalStatements/06ProductOfThreeNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿
+double num1 = double.Parse(Console.ReadLine());
+double num2 = double.Parse(Console.ReadLine());
+double num3 = double.Parse(Console.ReadLine());
+
+double add = num1 * num2 * num3;
+
+if (add > 0)
+{
+    Console.WriteLine("positive");
+}
+else if (add < 0)
+{
+    Console.WriteLine("negative");
+}
+else
+{
+    Console.WriteLine("zero");
+}
